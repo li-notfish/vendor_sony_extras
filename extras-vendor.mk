9a6151cc272f0219afb32decfd7e079e1dbafae7
onlyfish <linotfish@outlook.com>
2025-06-26 17:23:23 +0800

    first commit

---
+++ extras-vendor.mk
@@ -0,0 +1,77 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/sony/extras
+
+PRODUCT_COPY_FILES += \
+    vendor/sony/extras/proprietary/product/etc/permissions/com.sonyericsson.music.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.sonyericsson.music.xml \
+    vendor/sony/extras/proprietary/product/etc/permissions/jp.co.sony.mc.usbextoutaudio.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/jp.co.sony.mc.usbextoutaudio.xml \
+    vendor/sony/extras/proprietary/system_ext/etc/permissions/com.sonymobile.gesdklibrary.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.sonymobile.gesdklibrary.xml \
+    vendor/sony/extras/proprietary/vendor/etc/init/android.hardware.media.c2@1.0-threesixty-ra-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.media.c2@1.0-threesixty-ra-service.rc \
+    vendor/sony/extras/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
+    vendor/sony/extras/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc
+
+PRODUCT_PACKAGES += \
+    libXtcApi \
+    libaudiokeymgr \
+    libaudiosnpewrapper \
+    libaudiosnpewrapper_context \
+    libcodec2_soft_ac4dec \
+    libcodec2_soft_ddpdec \
+    libcodec2_soft_dolby \
+    libcodec2_soft_mha1dec \
+    libcodec2_soft_mhm1dec \
+    libcodec2_store_dolby \
+    libdapparamstorage \
+    libdeccfg \
+    libdlbdsservice \
+    libdlbpreg \
+    libiVptApi \
+    libiVptHkiDec \
+    libiVptLibC \
+    liboem_specific \
+    librebuffering \
+    libthreesixty_ra_codec2_store \
+    libtsrspkenhance \
+    libupmix-lib \
+    libznr \
+    libbundlewrapper1 \
+    libbundlewrapper2 \
+    libbundlewrapper3 \
+    libbundlewrapper4 \
+    libdlbvol \
+    libdnnrwrapper \
+    libswdap \
+    libswgamedap \
+    libswvqe \
+    libtsrspatializer \
+    libtsrupmix \
+    libznrwrapper \
+    vendor.dolby.hardware.dms@2.0-impl \
+    vendor.dolby.hardware.dms@2.0_vendor \
+    libmepstarterjni \
+    libmmparserextractor \
+    libavenhancements \
+    vendor.dolby.hardware.dms@2.0 \
+    CameraExtensionsProxy \
+    CameraAddonPermission \
+    CameraCommonPermission \
+    GamingAccessorySettings \
+    GamingFanService \
+    SomcCinemaPro \
+    SomcMediaExtensionProviderStarter \
+    SomcVideoPro \
+    ThermalFanService \
+    CameraCommon-Yodo \
+    CameraPanorama-Yodo \
+    SemcMusic \
+    SomcMediaExtensionProvider-Yodo \
+    SomcMovieCreator-AsahiYodo \
+    SomcPhotoPro \
+    UsbExtOutAudio \
+    com.sonymobile.gesdklibrary \
+    android.hardware.media.c2@1.0-threesixty-ra-service \
+    vendor.dolby.hardware.dms@2.0-service \
+    vendor.dolby.media.c2@1.0-service
